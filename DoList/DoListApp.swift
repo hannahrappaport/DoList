e9a088efa622636fb4472295dc4fe3f78d37207c
Hannah Rappaport <hannahgrappaport@gmail.com>
2024-05-09 13:13:59 -0400

    Initial Commit

---
+++ DoList/DoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoListApp.swift
+//  DoList
+//
+//  Created by Hannah Rappaport on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct DoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
